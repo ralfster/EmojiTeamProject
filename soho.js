53ac6e5732fae4df46ab5dd420eeaf5f33ecb31c
Ralf Dalby <32537467+ralfster@users.noreply.github.com>
2018-06-19 09:24:38 +0200

    Add files via upload

---
+++ soho.js
@@ -0,0 +1,24 @@
+document.getElementById("readmore1").onclick = function () {
+    foldUd()
+} 
+function foldUd() {
+    console.log("Fedt mann det virker!");
+    
+    document
+    .getElementById("historietekst")
+    .innerHTML = "- Indehaverne Hanne Møldrup Jensen og Hanne Møller fik arbejde hos Sophies tøjhus, da deres arbejdsliv startede. Ansættelsen udviklede sig til noget dybere for dem, end bare at sælge tøj. Sophies tøjhus havde en stor indflydelse på Hanne og Hanne i forhold til deres syn på modebranchen. Der blev taget chancer i Sophies tøjhus. Sophies tøjhus solgte både kvinde- og herretøj, men havde et smalt sortiment inden for herretøj. Det smalle sortiment af herretøj indeholdt arbejdstøj, mere specifikt kinaskjorter. Sophies tøjhus fandt tøjet stilløst, og ville prøve at udfordre stilen, ved at farve tøjet. Dette var en stor succes, og tøjet var eftertragtet af kunder. Kundesegmentet inden for herretøj voksede, og Sophies tøjhus så nøden til at udvide, så de kunne tilfredsstille deres kunder. SOHO blev grundlagt. Hanne og Hanne blev de ansvarsfulde for forretningen, hvor de skulle sælge herretøjet. Hanne og Hanne nød denne udvikling, og de havde nu ansvaret. Passionen voksede og herretøjet der blev solgt i SOHO var anderledes, end det man kunne finde andre steder i Aarhus. Efter en lang årrække fik Hanne og Hanne muligheden for at overtage SOHO fra Sophies tøjhus. De havde styret SOHO op til nu, og nød arbejdet inden for herretøjsbranchen. De havde bygget en unik forretning op, og havde været en del af at skabe noget nyt og anderledes inden for herremoden i Aarhus. De takkede ja til muligheden, og blev ved med at føre deres visioner frem igennem butikken. Disse visioner og tilgang til at føre forretning ses tydeligt i SOHO i dag.";
+
+}
+   
+/*----------------------------------------------------------------------*/
+document.getElementById("readmore2").onclick = function () {
+    foldUd
+
+    
+    document
+    .getElementById("tekstkollage")
+    .innerHTML = "- SOHO vil være anderledes! At skille sig ud fra mængden og tilbyde ens kunder noget unikt i form af produkter og oplevelser er drivkraften bag SOHO. SOHO tager imod dig med åbne arme, og stræber efter at gøre din oplevelse hos SOHO til noget unikt. SOHO tilbyder ”hele pakken”, i form af kvalitetsprodukter, personlig og videns rig service og et bredt sortiment til enhver smag. Ud over dette, byder SOHO dig velkommen ind i varme og trygge rammer. Forretningen er præget af en afslappende stemning, som får dig til at føle dig hjemme.";
+}
+
+/*--------------------------------------------------------------------*/
+
